30bf2b744ba79668c4e01c0f93f23cc4e67a9891
Joanne Qiong Sun <joannesun44@gmail.com>
2020-03-15 20:06:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transfer
   
   attr_accessor :sender, :receiver, :status, :amount
   
-  def initialize(amount,sender, receiver)
+  def initialize(amount, sender, receiver)
   @amount = amount
   @sender = sender
   @receiver = receiver
